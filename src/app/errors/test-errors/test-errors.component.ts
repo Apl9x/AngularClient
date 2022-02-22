9b99ca5094cf4c7ad1f9c4e16bcbfcb646eb3e13
Alejandro Perez <crvv002@gmail.com>
2022-02-22 07:45:28 -0600

    Error 404 Not Found

--- src/app/errors/test-errors/test-errors.component.ts
+++ src/app/errors/test-errors/test-errors.component.ts
@@ -7,51 +7,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-errors.component.css']
 })
 export class TestErrorsComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/'
+  baseUrl = 'https://localhost:5001/api/';
+  validationErrors: string[];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  get400Error(){
-    this.http.get(this.baseUrl+'buggy/bad-request').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+  get400Error() {
+
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+
+      next: (response) => console.log(response),
+
+      error: (error) => console.log(error)
+
+    });
+
   }
 
-  get400ValidationError(){
-    this.http.get(this.baseUrl+'buggy/bad-request').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+
+
+  get400ValidationError() {
+
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+
+      next: (response) => console.log(response),
+
+      error: (error) => {
+
+        console.log(error);
+
+        this.validationErrors = error;
+
+      }
+
+    });
+
   }
 
-  get401Error(){
-    this.http.get(this.baseUrl+'buggy/auth').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+
+
+  get401Error() {
+
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+
+      next: (response) => console.log(response),
+
+      error: (error) => console.log(error)
+
+    });
+
   }
 
-  get404Error(){
-    this.http.get(this.baseUrl+'buggy/not-found').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+  get404Error() {
+
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+
+      next: (response) => console.log(response),
+
+      error: (error) => console.log(error)
+
+    });
+
   }
 
-  get500Error(){
-    this.http.get(this.baseUrl+'buggy/server-error').subscribe(response=>{
-      console.log(response);
-    },error=>{
-      console.log(error);
-    })
+
+
+  get500Error() {
+
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+
+      next: (response) => console.log(response),
+
+      error: (error) => console.log(error)
+
+    });
+
   }
 
 }
