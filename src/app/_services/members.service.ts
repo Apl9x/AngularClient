b13f5d63a68745df3c79c3a8276851e64fd7f2d3
Alejandro Perez <crvv002@gmail.com>
2022-03-10 07:49:25 -0600

    members to Observable and consistant

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -10,22 +11,37 @@ import { Member } from '../_models/member';
 // }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+"users");
+  getMembers(): Observable<Member[]> {
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map((members) => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
-  getMember(username: string){
-    return this.http.get<Member>(this.baseUrl+"users/"+username);
+  getMember(username: string): Observable<Member> {
+    const member = this.members.find((x) => x.userName === username);
+    if (member != undefined) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  updateMember(member: Member){
-    return this.http.put(this.baseUrl+"users",member);
+  updateMember(member: Member): Observable<void> {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
   }
 }
