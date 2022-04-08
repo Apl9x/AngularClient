fe4c149e62a8c3fee3ed20a75f8183fa83e73ebf
Alejandro Perez <crvv002@gmail.com>
2022-04-08 06:56:38 -0500

    Filters on matches

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -10,34 +10,37 @@ import { MembersService } from 'src/app/_services/members.service';
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
-  styleUrls: ['./member-list.component.css'],
+  styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
   members: Member[];
   pagination: Pagination;
   userParams: UserParams;
   user: User;
-  genderList: [{value: "female",display: "Females"},{value: "male", display: "Males"}]
+  genderList = [{value: "female", display: "Females"}, {value: "male", display: "Males"}] 
 
-  constructor(private membersService: MembersService,private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>{
+  constructor(private memberService: MembersService, private accountService: AccountService) { 
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
       this.user = user;
       this.userParams = new UserParams(user);
-    })
+    });
   }
 
   ngOnInit(): void {
     this.loadMembers();
   }
 
-  loadMembers(): void{
-    this.membersService.getMembers(this.userParams).subscribe(response=>{
-      this.members = response.result;
-      this.pagination = response.pagination;
-    });
+  loadMembers(){
+    this.memberService.getMembers(this.userParams).subscribe(
+      response =>{
+        this.members = response.result;
+        this.pagination = response.pagination;
+      }
+    );
   }
-  
-  pageChanged(event: any): void{
+
+
+  pageChanged(event: any):void{
     this.userParams.pageNumber = event.page;
     this.loadMembers();
   }
