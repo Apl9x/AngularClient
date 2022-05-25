d0b13cfd855a55facf658ca5ca7ace9fc6deac24
Alejandro Perez <crvv002@gmail.com>
2022-05-25 07:53:35 -0500

    Send messages functionality

--- src/app/_services/message.service.ts
+++ src/app/_services/message.service.ts
@@ -8,20 +8,25 @@ import { PaginatedResult } from '../_models/pagination';
 
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MessageService {
-  baseUrl = environment.apiUrl;
+    baseUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient){}
+    constructor(private http: HttpClient){}
 
-  getMessages(pageNumber: number, pageSize: number, container: string): Observable<PaginatedResult<Message[]>>{
-    let params = getPaginationHeaders(pageNumber,pageSize);
-    params=params.append("Container", container);
-    return getPaginatedResult<Message[]>(this.baseUrl + "messages",params, this.http);
-  }
+    getMessages(pageNumber: number, pageSize: number, container: string): Observable<PaginatedResult<Message[]>>{
+        let params = getPaginationHeaders(pageNumber,pageSize);
+        params=params.append("Container", container);
+        return getPaginatedResult<Message[]>(this.baseUrl + "messages",params, this.http);
+    }
 
-  getMessageThread(username: string): Observable<Message[]>{
-    return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username);
-  }
+    getMessageThread(username: string): Observable<Message[]>{
+        return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username);
+    }
+
+    sendMessage(username: string, content: string): Observable<Message> {
+        return this.http.post<Message>(this.baseUrl + "messages", {recipientUsername: username, content});
+
+    }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -11,9 +11,10 @@ import { MessageService } from '../_services/message.service';
 export class MessagesComponent implements OnInit {
   messages: Message[] = [];
   pagination: Pagination;
-  container: "Inbox";
+  container: "Unread";
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService: MessageService) { }
 
@@ -22,13 +23,15 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages(): void{
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber, this.pageSize,this.container).subscribe(response=>{
       this.messages = response.result;
       this.pagination = response.pagination;
-    })
+      this.loading = false;
+    });
   }
 
-  pageChanged(event: any):void{
+  pageChanged(event: any): void{
     if(this.pageNumber !== event.page){
       this.pageNumber = event.page;
       this.loadMessages();
